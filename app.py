4de6a1585765b40f0a8355ad0fbbb091af280408
rghv96 <raghavsethi96@gmail.com>
2016-08-03 22:41:17 +0530

    temp

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def webook():
                             quotes.append(quote.contents[0])
                         random.shuffle(quotes)
                         result = quotes[:1]   
-                        send_message(sender_id, result)
+                        send_message(sender_id, str(result))
                         #send_message(sender_id, str(pybrainyquote.get_random_quote()))
 
                     else :
