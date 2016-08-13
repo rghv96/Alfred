68761c26ab63019ef3d78f2170ecb9104477b81d
rghv96 <raghavsethi96@gmail.com>
2016-08-13 16:13:41 +0530

    temp

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def send_message(recipient_id, message_text):
         "message": {
             "text": message_text
         },
-        #"sender_action":"typing_on"
+        "sender_action":"typing_off"
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
