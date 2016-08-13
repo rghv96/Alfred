ac0d8c5a185779669cfb3bc9ebc101dca32e444e
rghv96 <raghavsethi96@gmail.com>
2016-08-13 16:46:35 +0530

    temp

--- app.py
+++ app.py
@@ -65,24 +65,24 @@ def webook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    #message_text = messaging_event["message"]["text"]  # the message's text
 
-                    if len(message_text)>0:
-                        nameRegex = re.compile(r'quote (.*)')
-                        mo = nameRegex.search(message_text.lower())
+                    #if len(message_text)>0:
+                    #    nameRegex = re.compile(r'quote (.*)')
+                    #    mo = nameRegex.search(message_text.lower())
 
-                        if message_text.lower() == 'hi' or message_text.lower() == 'hey' or message_text.lower() == 'hello' or message_text.lower() == 'yo':
-                            type_message(sender_id)
-                            send_message(sender_id, "Hello there :)")
-                        elif message_text.lower() == 'quote': 
-                            type_message(sender_id)
-                            send_message(sender_id, str(get_random_quote()))
+                    #    if message_text.lower() == 'hi' or message_text.lower() == 'hey' or message_text.lower() == 'hello' or message_text.lower() == 'yo':
+                    #        type_message(sender_id)
+                    #        send_message(sender_id, "Hello there :)")
+                    #    elif message_text.lower() == 'quote': 
+                    #        type_message(sender_id)
+                    #        send_message(sender_id, str(get_random_quote()))
                         
-                        elif mo != None :
-                            send_message(sender_id, str(get_quotes(mo.group(1))))
-                        else :
-                            type_message(sender_id)
-                            send_message(sender_id, "type <quote> to get a random quote and <quote> <topic> to get a quote related to the topic :)")
+                    #    elif mo != None :
+                    #        send_message(sender_id, str(get_quotes(mo.group(1))))
+                    #    else :
+                    #        type_message(sender_id)
+                    #        send_message(sender_id, "type <quote> to get a random quote and <quote> <topic> to get a quote related to the topic :)")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
